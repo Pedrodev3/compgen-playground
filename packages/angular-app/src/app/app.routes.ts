5c22a37253d236e977b2bd199cab427942cb6c50
Pedro Victor <pedrosaraivadev@gmail.com>
2025-10-01 13:54:38 -0300

    Feat: Define appRoutes with lazy-loaded components for button features in Angular

--- packages/angular-app/src/app/app.routes.ts
+++ packages/angular-app/src/app/app.routes.ts
@@ -1,3 +1,32 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
-export const appRoutes: Routes = [];
+export const appRoutes: Routes = [
+  {
+    path: "",
+    loadComponent: () =>
+      import("./components/compgen/button-cli/buttonteste-cli.component").then(
+        (m) => m.ButtonTestCliComponent
+      ),
+  },
+  {
+    path: "compgen-button-fe",
+    loadComponent: () =>
+      import("./components/compgen/button-fe/buttonfe-button.component").then(
+        (m) => m.buttonFeButtonComponent
+      ),
+  },
+  {
+    path: "compgen-button-cp",
+    loadComponent: () =>
+      import("./components/compgen/button-cp/buttoncp-button.component").then(
+        (m) => m.buttonCpButtonComponent
+      ),
+  },
+  {
+    path: "manual-button",
+    loadComponent: () =>
+      import("./components/manual/button-manual/button-manual.component").then(
+        (m) => m.ButtonManualComponent
+      ),
+  },
+];
